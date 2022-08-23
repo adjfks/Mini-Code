5c4b84a6c6c3a31ff4bac065faee447bc00e1928
corner <sptdfdzl@163.com>
2022-08-24 00:13:15 +0800

    #1, feat: 实现异步任务插队功能

--- code/async-pool/index.js
+++ code/async-pool/index.js
@@ -3,26 +3,48 @@ export default class AsyncPool {
     this.limit = limit
     this.taskQueue = [].concat(tasks)
     this.executingQueue = []
+    this.scheduling = false
     this.startSchedule()
   }
 
   async startSchedule() {
+    this.scheduling = true
     while (this.taskQueue.length) {
-      const task = this.taskQueue.shift()
       if (this.executingQueue.length >= this.limit) {
         console.log('开始等待...')
         await Promise.race(this.executingQueue)
         console.log('等待结束...')
       }
-      const executingTask = Promise.resolve(task()).then(() =>
-        this.executingQueue.splice(
-          this.executingQueue.indexOf(executingTask),
-          1
-        )
-      )
+      const task = this.taskQueue.shift()
+      const executingTask = this.resolveTask(task)
       this.executingQueue.push(executingTask)
       console.log('加入一个执行任务')
     }
-    Promise.all(this.executingQueue).then(() => console.log('全部执行完毕'))
+    if (this.executingQueue.length) {
+      Promise.all(this.executingQueue).then(() => {
+        console.log('全部执行完毕')
+        this.scheduling = false
+      })
+    }
+  }
+
+  resolveTask(task) {
+    const executingTask = Promise.resolve(task()).then(() =>
+      this.executingQueue.splice(this.executingQueue.indexOf(executingTask), 1)
+    )
+    return executingTask
+  }
+
+  insertTask(task, index) {
+    this.taskQueue.splice(index, 0, task)
+    if (!this.scheduling) this.startSchedule()
+  }
+
+  unshiftTask(task) {
+    this.insertTask(task, 0)
+  }
+
+  appendTask(task) {
+    this.insertTask(task, this.taskQueue.length)
   }
 }
