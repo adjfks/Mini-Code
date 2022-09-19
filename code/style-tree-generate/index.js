7b6cde4074bf28d689ea6f9cb6c8525ef30db0ca
corner <sptdfdzl@163.com>
2022-09-19 23:48:33 +0800

    #18, fix: 删除旧代码

--- code/style-tree-generate/index.js
+++ code/style-tree-generate/index.js
@@ -1,21 +1,10 @@
 function getCssTree(componentSetLibNode) {
-  const cssTree = {
-    selector: '.' + componentSetLibNode.name,
-    styleBody: {},
-    children: [],
-  }
-  componentSetLibNode.propOtions.sort((a, b) => a.length - b.length)
-}
-
-module.exports = getStyle
-function getCssTree(componentSetLibNode) {
   const attrSelectors = getSelectors(componentSetLibNode.propOptions)
   const root = {
     selector: '.' + componentSetLibNode.name,
     styleBody: {},
     children: initChildren(attrSelectors, 0),
   }
-  console.log(root)
 }
 
 function initChildren(attrSelectors, i) {
