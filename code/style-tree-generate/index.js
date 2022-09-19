cfefddfe67a950f92121200bc570a86f3adcb81c
corner <sptdfdzl@163.com>
2022-09-19 23:47:44 +0800

    #18, feat: 完成组件库样式树初始化

--- code/style-tree-generate/index.js
+++ code/style-tree-generate/index.js
@@ -8,3 +8,41 @@ function getCssTree(componentSetLibNode) {
 }
 
 module.exports = getStyle
+function getCssTree(componentSetLibNode) {
+  const attrSelectors = getSelectors(componentSetLibNode.propOptions)
+  const root = {
+    selector: '.' + componentSetLibNode.name,
+    styleBody: {},
+    children: initChildren(attrSelectors, 0),
+  }
+  console.log(root)
+}
+
+function initChildren(attrSelectors, i) {
+  if (i >= attrSelectors.length) return []
+  const children = []
+  const cur = attrSelectors[i]
+  for (let j = 0, len = cur.length; j < len; j++) {
+    const selector = cur[j]
+    const css = {
+      selector,
+      styleBody: {},
+      children: initChildren(attrSelectors, i + 1),
+    }
+    children.push(css)
+  }
+  return children
+}
+
+function getSelectors(propOptions) {
+  const attrSelectors = []
+  for (const optionName in propOptions) {
+    const optionValues = propOptions[optionName].map(
+      (val) => `[${optionName}="${val}"]`
+    )
+    attrSelectors.push(optionValues)
+  }
+  return attrSelectors
+}
+
+module.exports = getCssTree
