2e655cef4ca68236b0c8aa61dbc013098354fb37
supeitao <supeitao@9realms.co>
2022-09-19 14:47:58 +0800

    #18, feat: 完成获取样式树公共部分函数编写

--- code/style-tree-generate/getCommonStyle.test.js
+++ code/style-tree-generate/getCommonStyle.test.js
@@ -1,47 +1,44 @@
 const getCommonStyle = require('./getCommonStyle')
-const { styleTree1, styleTree2 } = require('./data')
+const h = require('./h')
 
-const resTree = {
-  name: 'class01',
-  styles: {
-    color: 'red',
-    backgroundColor: '#add',
-  },
-  children: [
-    {
-      name: 'class02',
-      styles: {
-        height: '15px',
-      },
-      children: [],
-    },
-    {
-      name: 'class03',
-      styles: {
-        color: 'red',
-        height: '15px',
-      },
-      children: [
-        {
-          name: 'class04',
-          styles: {
-            textAlign: 'center',
-            backgroundColor: 'pink',
-          },
-          children: [],
-        },
-      ],
-    },
-    {
-      name: 'class05',
-      styles: {
-        height: '15px',
-      },
-      children: [],
-    },
-  ],
-}
+const styleTree1 = h('class01', { color: 'red', backgroundColor: 'green' }, {
+  class02: h('class02', { display: 'none' }),
+  class03: h('class03', { textAlign: 'center', display: 'flex', justifyContent: 'center' }, {
+    class04: h('class04', { flex: 1 }, {}),
+    class05: h('class05', { flex: 1 }, {
+      class06: h('class06', { lineHeight: '20px' }, {})
+    })
+  }),
+  class07: h('class07', { color: 'purple' }, {})
+})
+
+const styleTree2 = h('class01', { color: 'red' }, {
+  class02: h('class02', { color: 'pink', lineHeight: '2' }, {
+    class08: h('class08', { height: '40px' }, {}),
+    class09: h('class09', { display: 'none' }, {})
+  }),
+  class03: h('class03', { textAlign: 'center', display: 'flex', justifyContent: 'spaceBetween' }, {
+    class04: h('class04', { flex: 1 }, {}),
+    class05: h('class05', { flex: 1 }, {
+      class06: h('class06', { lineHeight: '21px' }, {})
+    })
+  }),
+  class07: h('class07', { color: 'purple' }, {})
+})
+
+const resTree = h('class01', { color: 'red' }, {
+  class03: h('class03', { textAlign: 'center', display: 'flex' }, {
+    class04: h('class04', { flex: 1 }, {}),
+    class05: h('class05', { flex: 1 }, {
+      class06: h('class06', {}, {})
+    })
+  }),
+  class07: h('class07', { color: 'purple' }, {})
+})
 
 test('样式树提取公共部分算法', () => {
-  expect(getCommonStyle(styleTree1, styleTree2)).toEqual(resTree)
+  const res = getCommonStyle(styleTree1, styleTree2)
+  console.log(res);
+  console.log(resTree);
+  expect(res).toEqual(resTree)
 })
---
+++ code/style-tree-generate/h.js
@@ -0,0 +1,9 @@
+function h(sel, styles, children = new Map()) {
+  return {
+    sel,
+    styles,
+    children
+  }
+}
+
+module.exports = h
