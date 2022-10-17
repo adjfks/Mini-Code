71481e457500dfd566634de744b7989e7382399b
corner <sptdfdzl@163.com>
2022-10-18 00:40:50 +0800

    feat: 实现最基础版effect

--- code/vue3-plain/packages/reactivity/src/baseHandler.ts
+++ code/vue3-plain/packages/reactivity/src/baseHandler.ts
@@ -1,3 +1,5 @@
+import { track, trigger } from './effect'
+
 export const enum ReactiveFlag {
   IS_REACTIVE = '__v_isReactive',
 }
@@ -7,10 +9,19 @@ export const mutableHandlers = {
     if (key === ReactiveFlag.IS_REACTIVE) {
       return true
     }
+
+    // 依赖收集
+    track(target, key)
+
     // reciver指向proxy，必须使用Reflect.get来获取属性并将this指向proxy
     return Reflect.get(target, key, receiver)
   },
   set(target, key, value, receiver) {
-    return Reflect.set(target, key, value, receiver)
+    const oldValue = receiver[key]
+    const res = Reflect.set(target, key, value, receiver)
+    if (oldValue !== value) {
+      trigger(target, key, value, oldValue)
+    }
+    return res
   },
 }
--- code/vue3-plain/packages/reactivity/src/effect.ts
+++ code/vue3-plain/packages/reactivity/src/effect.ts
@@ -1,1 +1,53 @@
-export function effect() {}
+export let activeEffect: ReactiveEffect | undefined = undefined
+
+type Dep = Set<ReactiveEffect>
+type keyToDepMap = Map<any, Dep>
+const targetMap = new WeakMap<any, keyToDepMap>()
+
+class ReactiveEffect {
+  active = true
+  constructor(public fn) {}
+  run() {
+    // 非激活情况，只需要执行函数，不需要进行依赖收集
+    if (!this.active) this.fn()
+    try {
+      activeEffect = this
+      this.fn()
+    } finally {
+      activeEffect = undefined
+    }
+  }
+}
+
+export function effect<T = any>(fn: () => T) {
+  const _effect = new ReactiveEffect(fn)
+  _effect.run()
+}
+
+// 依赖收集
+export function track(target: object, key) {
+  let keyToDepMap = targetMap.get(target)
+  if (!keyToDepMap) {
+    targetMap.set(target, (keyToDepMap = new Map()))
+  }
+  let Dep = keyToDepMap.get(key)
+  if (!Dep) {
+    keyToDepMap.set(key, (Dep = new Set()))
+  }
+  if (activeEffect) {
+    Dep.add(activeEffect)
+  }
+}
+
+// 触发更新
+export function trigger(target: object, key, value, oldValue) {
+  let effects = new Set<() => any>()
+  const keyToDepMap = targetMap.get(target)
+  const dep = keyToDepMap.get(key)
+  dep.forEach((e) => {
+    effects.add(e.run.bind(e))
+  })
+  effects.forEach((effect) => {
+    effect()
+  })
+}
