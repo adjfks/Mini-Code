2f6da41b9ae73fc2cf9a039c277b9740eb2da07f
corner <sptdfdzl@163.com>
2022-12-18 23:04:23 +0800

    #43-2, diff

--- code/vue-source/5-diff/diff.js
+++ code/vue-source/5-diff/diff.js
@@ -1,3 +1,135 @@
 /* 
 这里列出vue源码中diff相关的核心代码
 */
+function sameVnode(a, b) {
+  return (a.key === b.key &&
+    a.asyncFactory === b.asyncFactory &&
+    ((a.tag === b.tag &&
+      a.isComment === b.isComment &&
+      isDef(a.data) === isDef(b.data) &&
+      sameInputType(a, b)) ||
+      (isTrue(a.isAsyncPlaceholder) && isUndef(b.asyncFactory.error))));
+}
+
+function patch(oldVnode, vnode) {
+  var isRealElement = isDef(oldVnode.nodeType);
+  if (!isRealElement && sameVnode(oldVnode, vnode)) {
+    patchVnode(oldVnode, vnode)
+  }
+}
+
+function patchVnode(oldVnode, vnode) {
+  // 如果新节点不是text节点
+  if (isUndef(vnode.text)) {
+    // 新旧节点的children都存在且不相等，调用updateChildren进行比较
+    if (isDef(oldCh) && isDef(ch)) {
+      if (oldCh !== ch)
+        updateChildren(elm, oldCh, ch);
+    }
+    // 新节点children存在旧节点children不存在
+    else if (isDef(ch)) {
+      {
+        checkDuplicateKeys(ch);
+      }
+      // 旧节点的子节点是text则清除并插入新节点的子节点
+      if (isDef(oldVnode.text))
+        nodeOps.setTextContent(elm, '');
+      addVnodes(elm, null, ch, 0, ch.length - 1, insertedVnodeQueue);
+    }
+    // 旧节点存在children而新节点不存在，则删除旧节点的children
+    else if (isDef(oldCh)) {
+      removeVnodes(oldCh, 0, oldCh.length - 1);
+    }
+    // 新旧节点都没有children且旧节点有文本子节点而新节点既没有text也没有children,则删除旧的text
+    else if (isDef(oldVnode.text)) {
+      nodeOps.setTextContent(elm, '');
+    }
+  }
+  // text不同更新text
+  else if (oldVnode.text !== vnode.text) {
+    nodeOps.setTextContent(elm, vnode.text);
+  }
+}
+
+function updateChildren(parentElm, oldCh, newCh) {
+  var oldStartIdx = 0;
+  var newStartIdx = 0;
+  var oldEndIdx = oldCh.length - 1;
+  var oldStartVnode = oldCh[0];
+  var oldEndVnode = oldCh[oldEndIdx];
+  var newEndIdx = newCh.length - 1;
+  var newStartVnode = newCh[0];
+  var newEndVnode = newCh[newEndIdx];
+  var oldKeyToIdx, idxInOld, vnodeToMove, refElm;
+
+  while (oldStartIdx <= oldEndIdx && newStartIdx <= newEndIdx) {
+    if (isUndef(oldStartVnode)) {
+      oldStartVnode = oldCh[++oldStartIdx]; // Vnode has been moved left
+    }
+    else if (isUndef(oldEndVnode)) {
+      oldEndVnode = oldCh[--oldEndIdx];
+    }
+    else if (sameVnode(oldStartVnode, newStartVnode)) {
+      patchVnode(oldStartVnode, newStartVnode, insertedVnodeQueue, newCh, newStartIdx);
+      oldStartVnode = oldCh[++oldStartIdx];
+      newStartVnode = newCh[++newStartIdx];
+    }
+    else if (sameVnode(oldEndVnode, newEndVnode)) {
+      patchVnode(oldEndVnode, newEndVnode, insertedVnodeQueue, newCh, newEndIdx);
+      oldEndVnode = oldCh[--oldEndIdx];
+      newEndVnode = newCh[--newEndIdx];
+    }
+    else if (sameVnode(oldStartVnode, newEndVnode)) {
+      // Vnode moved right
+      patchVnode(oldStartVnode, newEndVnode, insertedVnodeQueue, newCh, newEndIdx);
+      canMove &&
+        nodeOps.insertBefore(parentElm, oldStartVnode.elm, nodeOps.nextSibling(oldEndVnode.elm));
+      oldStartVnode = oldCh[++oldStartIdx];
+      newEndVnode = newCh[--newEndIdx];
+    }
+    else if (sameVnode(oldEndVnode, newStartVnode)) {
+      // Vnode moved left
+      patchVnode(oldEndVnode, newStartVnode, insertedVnodeQueue, newCh, newStartIdx);
+      canMove &&
+        nodeOps.insertBefore(parentElm, oldEndVnode.elm, oldStartVnode.elm);
+      oldEndVnode = oldCh[--oldEndIdx];
+      newStartVnode = newCh[++newStartIdx];
+    }
+    else {
+      if (isUndef(oldKeyToIdx))
+        oldKeyToIdx = createKeyToOldIdx(oldCh, oldStartIdx, oldEndIdx);
+      idxInOld = isDef(newStartVnode.key)
+        ? oldKeyToIdx[newStartVnode.key]
+        : findIdxInOld(newStartVnode, oldCh, oldStartIdx, oldEndIdx);
+      if (isUndef(idxInOld)) {
+        // New element
+        createElm(newStartVnode, insertedVnodeQueue, parentElm, oldStartVnode.elm, false, newCh, newStartIdx);
+      }
+      else {
+        vnodeToMove = oldCh[idxInOld];
+        if (sameVnode(vnodeToMove, newStartVnode)) {
+          patchVnode(vnodeToMove, newStartVnode, insertedVnodeQueue, newCh, newStartIdx);
+          oldCh[idxInOld] = undefined;
+          canMove &&
+            nodeOps.insertBefore(parentElm, vnodeToMove.elm, oldStartVnode.elm);
+        }
+        else {
+          // same key but different element. treat as new element
+          createElm(newStartVnode, insertedVnodeQueue, parentElm, oldStartVnode.elm, false, newCh, newStartIdx);
+        }
+      }
+      newStartVnode = newCh[++newStartIdx];
+    }
+  }
+  if (oldStartIdx > oldEndIdx) {
+    refElm = isUndef(newCh[newEndIdx + 1]) ? null : newCh[newEndIdx + 1].elm;
+    addVnodes(parentElm, refElm, newCh, newStartIdx, newEndIdx, insertedVnodeQueue);
+  }
+  else if (newStartIdx > newEndIdx) {
+    removeVnodes(oldCh, oldStartIdx, oldEndIdx);
+  }
+}
+
+function isDef(v) {
+  return v !== undefined && v !== null;
+}
