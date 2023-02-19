bc389c8dcd96f8d2fa4ff7c9889efaa5197b2942
corner <sptdfdzl@163.com>
2023-02-19 20:49:56 +0800

    #38-1, 快排注释

--- code/sort/quicksort.js
+++ code/sort/quicksort.js
@@ -11,7 +11,7 @@ function quicksort(l, r, arr) {
   //   swap(l, idx, arr)
   const pivot = arr[l]
   let i = l + 1,
-    x = l,
+    x = l,  // 左边界为l，不是l+1，因为需要把这个数排到正确位置
     y = r
   while (i <= y) {
     if (arr[i] <= pivot) {
@@ -20,7 +20,7 @@ function quicksort(l, r, arr) {
       swap(i, y--, arr)
     }
   }
-  quicksort(l, x - 1, arr)
+  quicksort(l, x - 1, arr)  // 注意：x位置已经排好，不需要再排
   quicksort(x + 1, r, arr)
 }
 
