f711df3c7d11b05e4161f60e993752882e109fe7
corner <sptdfdzl@163.com>
2024-04-06 16:00:27 +0800

    feat:新增sidebarNoteList组件

--- code/next-react-notes/README.md
+++ code/next-react-notes/README.md
@@ -1,6 +1,16 @@
 # next-react-notes
 
+## dev
+
 ```shell
 npm run dev
 redis-server
 ```
+
+
+
+## Note
+
+1. SidebarNoteList组件使用的是服务端渲染，dayjs这种库不会打包到客户端的bundle中。
+
+因此使用服务端组件的好处之一就是对客户端bundle size没有影响。
--- code/next-react-notes/components/Sidebar.js
+++ code/next-react-notes/components/Sidebar.js
@@ -1,7 +1,10 @@
 import React from 'react'
 import Link from 'next/link'
+import { getAllNotes } from '@/lib/redis'
+import SidebarNoteList from './SidebarNoteList'
 
-export default function Sidebar() {
+export default async function Sidebar() {
+  const notes = await getAllNotes()
   return (
     <>
       <section className="col sidebar">
@@ -18,7 +21,9 @@ export default function Sidebar() {
             <strong>React Notes</strong>
           </section>
         </Link>
-        <section className="sidebar-menu" role="menubar"></section>
+        <section className="sidebar-menu" role="menubar">
+          <SidebarNoteList notes={notes} />
+        </section>
         <nav></nav>
       </section>
     </>
